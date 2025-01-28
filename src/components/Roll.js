c6a8c665ea2c80b6144d8d0936e4085abd745c68
prodpeak <dtudor01@gmail.com>
2025-01-28 11:30:10 +0000

    Convert Roll to webp

--- src/components/Roll.js
+++ src/components/Roll.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import Marquee5 from './Marquee5'
-import monsters from '../assets/monsters.png'
+import monsters from '../assets/monsters.webp'
+
 function Roll() {
     return (
         <>
             <div className='flex flex-col w-full justify-center items-center px-4s my-20 overflow-y-clip lg:py-40 py-10'>
-                {/* <img src={mutants} alt="mutants" className='w-full lg:my-20 my-10' /> */}
                 <img src={monsters} alt="monsters" className='w-full -rotate-6' />
                 <div className="marq -rotate-6">
-                    {/* <Marquee4 /> */}
                     <Marquee5 />
                 </div>
 
