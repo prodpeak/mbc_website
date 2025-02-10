8d2081b7211ab1e005ad012b3d83ed6417450b44
prodpeak <dtudor01@gmail.com>
2025-02-10 14:32:51 +0000

    Switch birddog

--- src/components/roadmap/Birddog.js
+++ src/components/roadmap/Birddog.js
@@ -4,7 +4,7 @@ import birddog from '../../assets/birddog.webp'
 const Birddog = () => {
     return (
         <div className="flex flex-col lg:flex-row birddogbg w-full items-center pb-4 lg:pb-0">
-            <div className="order-1 lg:order-3 w-full lg:w-1/2 self-end">
+            <div className="order-1 lg:order-1 w-full lg:w-1/2 self-end">
                 <img src={birddog} alt="Birddog" className='w-full'/>
             </div>
 
